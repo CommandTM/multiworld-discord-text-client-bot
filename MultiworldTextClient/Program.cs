92c7c27b95c311915058bab988c565568e60eac7
CommandTM <commandnoobmc23@gmail.com>
2025-10-16 13:58:14 -0400

    Initial Commit

---
+++ MultiworldTextClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+
+namespace MultiworldTextClient;
+
+class Program
+{
+    private static DiscordSocketClient _client;
+    
+    static async Task Main(string[] args)
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+        _client.Ready += Ready;
+        _client.SlashCommandExecuted += ProcessSlashCommand;
+
+        var token = ""; // TODO: Make this pull from an app settings
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+
+    private static async Task ProcessSlashCommand(SocketSlashCommand arg)
+    {
+        switch (arg.Data.Name)
+        {
+            case "say":
+                await arg.RespondAsync("Hello!");
+                break;
+        }
+    }
+
+    private static async Task Ready()
+    {
+        var globalCommand = new SlashCommandBuilder()
+            .WithName("say")
+            .WithDescription("Responds With Something");
+        
+        await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+    }
+
+    private static async Task Log(LogMessage arg)
+    {
+        Console.WriteLine(arg.Message);
+    }
+}
